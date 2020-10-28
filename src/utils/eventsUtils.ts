c85b8feec0cbde586e923b624eff4db3251be7ca
John <john@trabian.com>
2020-10-28 01:46:58 -0400

    Updates event utilities for recurring events

--- src/utils/eventsUtils.ts
+++ src/utils/eventsUtils.ts
@@ -1,24 +1,31 @@
-import { daysAreEqual } from "./sharedUtils"
-import { CalendarEvent, Month, Day } from "../types"
+import { daysAreEqual } from './sharedUtils';
+import { CalendarEvent, Month, Day } from '../types';
+import { msInAnHour } from '../data';
 
-const getMonthEvents = (events: CalendarEvent[], month: Month, year: number) => {
-  const eventMonth = (event: CalendarEvent) => event.startDate.getMonth()
+const getMonthEvents = <E>(
+  events: CalendarEvent<E>[],
+  month: Month,
+  year: number
+) => {
+  const eventMonth = (event: CalendarEvent<E>) => event.startDate.getMonth();
 
-  const eventYear = (event: CalendarEvent) => event.startDate.getFullYear()
+  const eventYear = (event: CalendarEvent<E>) => event.startDate.getFullYear();
 
   return events.filter(
-    (event) => eventMonth(event) === month.id && eventYear(event) === year,
-  )
-}
+    (event) => eventMonth(event) === month.id && eventYear(event) === year
+  );
+};
 
-const getDayEvents = (events: CalendarEvent[], day: Day) => {
-  return events.filter((event) => daysAreEqual(event.startDate, day.date))
-}
+const getDayEvents = <E>(events: CalendarEvent<E>[], day: Day) => {
+  return events.filter((event) => daysAreEqual(event.startDate, day.date));
+};
 
-const getHourEvents = (events: CalendarEvent[], start: Date) => {
+const getHourEvents = <E>(events: CalendarEvent<E>[], start: Date) => {
   return events.filter((event) => {
-    return daysAreEqual(event.startDate, start) && event.startTimeHours === start.getHours()
-  })
-}
+    const eventStartH = Math.floor(Number(event.startDate) / msInAnHour);
+    const startH = Math.floor(Number(start) / msInAnHour);
+    return eventStartH === startH;
+  });
+};
 
-export {getMonthEvents, getHourEvents, getDayEvents}
+export { getMonthEvents, getHourEvents, getDayEvents };
