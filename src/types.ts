59526247377b273821948d7a83d77869fb6b311f
John <john@trabian.com>
2020-10-28 01:46:21 -0400

    Update Types for recurring events

--- src/types.ts
+++ src/types.ts
@@ -1,101 +1,150 @@
-type CSS = React.CSSProperties
+type CSS = React.CSSProperties;
 
 type TimePeriodLabel = {
-  name: string
-  shortName: string
-  id: number
-}
+  name: string;
+  shortName: string;
+  id: number;
+};
 
-type Month = TimePeriodLabel
+type Month = TimePeriodLabel;
 
-type DayOfWeek = TimePeriodLabel
+type DayOfWeek = TimePeriodLabel;
 
 type Day = {
-  date: Date
-  weekday: DayOfWeek
-  currentPeriod: boolean
-}
+  date: Date;
+  weekday: DayOfWeek;
+  currentPeriod: boolean;
+};
 
 type Week = {
   id: number;
   firstDay: Date;
-}
+};
 
-type CalendarEvent = {
-  id: string
-  title: string
-  allDay: boolean
-  description?: string
-  startDate: Date
-  endDate: Date
-  startTimeHours: number
-  startTimeMinutes: number
-  endTimeHours: number
-  endTimeMinutes: number
-}
+type InclusionRule = {
+  frequency: RecurranceFrequency;
+  interval?: number;
+  weekdays?: number[];
+  limit?: number;
+  serial: string;
+};
 
-type NewCalendarEvent = Omit<CalendarEvent, "id">
+type ExclusionRule = {
+  date: Date;
+};
+
+type Schedule = {
+  recurs: boolean;
+  inclusion: InclusionRule[];
+  exclusion?: ExclusionRule[];
+};
+
+type BaseCalendarEvent = {
+  id: string;
+  title: string;
+  allDay: boolean;
+  description?: string;
+  startDate: Date;
+  endDate: Date;
+};
+
+type CalendarEvent<E> = E & BaseCalendarEvent;
+
+type RecurringCalendarEvent<E> = CalendarEvent<E> & {
+  schedule: Schedule;
+};
+
+type NewCalendarEvent<E> = E & Omit<BaseCalendarEvent, 'id'>;
+
+type NewRecurringCalendarEvent<E> = Omit<CalendarEvent<E>, 'id'> & {
+  schedule: Schedule;
+};
 
 type DayStyleOptions = {
-  currentDayStyles?: CSS,
-  pastDayStyles?: CSS,
-  todayStyles?: CSS,
-  futureDayStyles?: CSS
-}
+  currentDayStyles?: CSS;
+  pastDayStyles?: CSS;
+  todayStyles?: CSS;
+  futureDayStyles?: CSS;
+};
 
 enum DisplayMode {
   month,
   week,
-  day
+  day,
 }
 
 type UseCalendar = {
-  today: Day,
-  months: Month[],
-  weekdays: DayOfWeek[],
-  currentDay: Day,
-  currentWeek: Week,
-  currentYear: number,
-  currentMonth: Month,
-  monthDisplayDays: Day[],
-  weekDisplayDays: Day[],
-  msInAMinute: number,
-  msInAnHour: number,
-  msInADay: number,
-  msInAWeek: number,
-  selectDay: (day: Day) => void,
-  getDayStyle: (day: Day, currentDay: Day, options?: DayStyleOptions | undefined) => CSS,
-  getFirstDayOfWeek: (Date: Date) => Date,
-  getNextWeek: () => Week,
-  getLastWeek: () => Week,
-  loadPrevWeek: () => void,
-  loadNextWeek: () => void,
-  getDaysInMonth: (m: number, y: number) => number,
-  getLastMonth: () => Month,
-  getNextMonth: () => Month,
-  loadPrevMonth: () => void,
-  loadNextMonth: () => void,
-  getWeekByDate: (date: Date) => Week,
-  getMonthFromDate: (date: Date) => Month,
-  getFirstDayOfYear: (year: number) => Date,
-  getLastDayOfYear: (year: number) => Date,
-  getFirstWeekOfYear: (year: number) => Week,
-  getWeekdayFromWeek: (week: Week, weekday: DayOfWeek) => Day,
-  getWeekNumber: (date: Date) => number,
-}
+  today: Day;
+  months: Month[];
+  weekdays: DayOfWeek[];
+  currentDay: Day;
+  currentWeek: Week;
+  currentYear: number;
+  currentMonth: Month;
+  monthDisplayDays: Day[];
+  weekDisplayDays: Day[];
+  msInAMinute: number;
+  msInAnHour: number;
+  msInADay: number;
+  msInAWeek: number;
+  selectDay: (day: Day) => void;
+  getDayStyle: (
+    day: Day,
+    currentDay: Day,
+    options?: DayStyleOptions | undefined
+  ) => CSS;
+  getFirstDayOfWeek: (Date: Date) => Date;
+  getNextWeek: () => Week;
+  getLastWeek: () => Week;
+  loadPrevWeek: () => void;
+  loadNextWeek: () => void;
+  getDaysInMonth: (m: number, y: number) => number;
+  getLastMonth: () => Month;
+  getNextMonth: () => Month;
+  loadPrevMonth: () => void;
+  loadNextMonth: () => void;
+  getWeekByDate: (date: Date) => Week;
+  getMonthFromDate: (date: Date) => Month;
+  getFirstDayOfYear: (year: number) => Date;
+  getLastDayOfYear: (year: number) => Date;
+  getFirstWeekOfYear: (year: number) => Week;
+  getWeekdayFromWeek: (week: Week, weekday: DayOfWeek) => Day;
+  getWeekNumber: (date: Date) => number;
+};
 
-type UseEvents = {
-  events: CalendarEvent[];
-  setEvents: React.Dispatch<React.SetStateAction<CalendarEvent[]>>;
-  addEvent: (event: NewCalendarEvent) => void;
-  updateEvent: (event: CalendarEvent) => void;
-  deleteEvent: (event: CalendarEvent) => void;
-  getHourEvents: (events: CalendarEvent[], start: Date) => CalendarEvent[];
-  getDayEvents: (events: CalendarEvent[], day: Day) => CalendarEvent[];
-  getMonthEvents: (events: CalendarEvent[], month: Month, year: number) => CalendarEvent[];
-}
+type UseEvents<E> = {
+  events: (CalendarEvent<E> | RecurringCalendarEvent<E>)[];
+  setEvents: React.Dispatch<
+    React.SetStateAction<(CalendarEvent<E> | RecurringCalendarEvent<E>)[]>
+  >;
+  addEvent: (event: NewCalendarEvent<E> | NewRecurringCalendarEvent<E>) => void;
+  updateEvent: (event: CalendarEvent<E> | RecurringCalendarEvent<E>) => void;
+  deleteEvent: (event: CalendarEvent<E> | RecurringCalendarEvent<E>) => void;
+  getHourEvents: (
+    events: CalendarEvent<E>[],
+    start: Date
+  ) => CalendarEvent<E>[];
+  getDayEvents: (events: CalendarEvent<E>[], day: Day) => CalendarEvent<E>[];
+  getMonthEvents: (
+    events: CalendarEvent<E>[],
+    month: Month,
+    year: number
+  ) => CalendarEvent<E>[];
+};
 
-type UseEventsCalendar = {calendar: UseCalendar,  calendarEvents: UseEvents }
+type UseRecurringEvent<E> = {
+  between: (start: Date, end: Date) => CalendarEvent<E>[];
+};
+
+type UseEventsCalendar<E> = {
+  calendar: UseCalendar;
+  calendarEvents: UseEvents<E>;
+  useRecurringEvent: (
+    event: RecurringCalendarEvent<E>
+  ) => {
+    between: (start: Date, end: Date) => RecurringCalendarEvent<E>[];
+  };
+};
 
 export type {
   CSS,
@@ -104,12 +153,27 @@ export type {
   Day,
   DayOfWeek,
   CalendarEvent,
+  RecurringCalendarEvent,
   NewCalendarEvent,
+  NewRecurringCalendarEvent,
+  Schedule,
+  InclusionRule,
+  ExclusionRule,
   DayStyleOptions,
   UseCalendar,
   UseEvents,
-  UseEventsCalendar
+  UseRecurringEvent,
+  UseEventsCalendar,
+};
+
+export { DisplayMode };
+
+export enum RecurranceFrequency {
+  YEARLY = 'YEARLY',
+  MONTHLY = 'MONTHLY',
+  WEEKLY = 'WEEKLY',
+  DAILY = 'DAILY',
+  HOURLY = 'HOURLY',
+  MINUTELY = 'MINUTELY',
+  SECONDLY = 'SECONDLY',
 }
-
-export {DisplayMode}
-
